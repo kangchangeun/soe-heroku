d64e4279c7de836df5404f1a909df0062328b7a3
kangchanggeun <kangchangeun@gmail.com>
2020-11-24 21:40:14 +0900

    the first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SoE - Demo app - GOGO Huyng ^^
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
